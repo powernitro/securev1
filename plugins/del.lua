e8d81d05bfaefa39631caf16745f4c2457a5c9cc
powernitro <powernitrobot@gmail.com>
2017-02-03 15:12:33 +0330

    Update del.lua

--- plugins/del.lua
+++ plugins/del.lua
@@ -9,7 +9,7 @@ local function run(msg, matches)
     if msg.chat_id_:match("^-100") then
        if is_sudo(msg) then
           if tonumber(matches[2]) > 100 or tonumber(matches[2]) < 1 then
-             pm = '❗️_ 100> تعداد پيام هاي قابل حذف هر دفعه >1 _❗️'
+             pm = '_ 100> تعداد پيام هاي قابل حذف هر دفعه >1 _'
              tdcli.sendMessage(msg.chat_id_, data.msg.id_, 1, pm, 1, 'html')
              else
           tdcli_function ({
@@ -19,7 +19,7 @@ local function run(msg, matches)
     offset_ = 0,
     limit_ = tonumber(matches[2])
   }, delmsg, nil)
-             pm ='`'..matches[2]..'` پيام اخير پاک شد🗑'
+             pm ='<code>'..matches[2]..'</code> پيام اخير پاک شد'
              tdcli.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
          end
      end
