b03b0a0a78484a899a26793b3c0dd9cad44e2fb0
powernitro <powernitrobot@gmail.com>
2017-02-07 15:55:02 +0330

    Update and rename beyond.sh to secure.sh

--- secure.sh
+++ secure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/BDReborn
+cd $HOME/secure
 
 install() {
 	    cd tg
@@ -31,13 +31,13 @@ if [ ! -f ./tg/tgcli ]; then
     exit 1
 fi
    echo -e "\033[38;5;208m"
-   echo -e "     > BeyondTeam Source :D                        "
+   echo -e "     > secure Source :D                        "
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
    ./tg/tgcli -s ./bot/bot.lua $@
 fi
 
-# Now All Argument Support after ./beyond.sh !
+# Now All Argument Support after ./secure.sh !
 #	Arguments :
 #			#			#			#			#			#			#			#			#			#
 #  --phone/-u                           specify username (would not be asked during authorization)
@@ -67,8 +67,8 @@ fi
 #  --permanent-peer-ids                 use permanent peer ids
 #			#			#			#			#			#			#			#			#			#
 #Example To launch with second profile :
-# ./beyond.sh -p second-profile
+# ./secure.sh -p second-profile
 
 #			  OR
 
-# ./beyond.sh --profile second-profile
+# ./secure.sh --profile second-profile
